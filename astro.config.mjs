ec231509f23b243b5320022954eeaf1ce1a7846d
jonno-lolcow <freshydocket@gmail.com>
2026-01-31 00:55:05 +0000

    latest changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://tranquil-queijadas-f4d3bb.netlify.app/",
   vite: {
     plugins: [tailwindcss()],
   },
