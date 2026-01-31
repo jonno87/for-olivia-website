44820655c4059dcc8ba34096e5a78c5e7575e3ca
jonno-lolcow <freshydocket@gmail.com>
2026-01-31 10:21:53 +0000

    SEO: site url, meta, og image, robots, sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://tranquil-queijadas-f4d3bb.netlify.app/",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
